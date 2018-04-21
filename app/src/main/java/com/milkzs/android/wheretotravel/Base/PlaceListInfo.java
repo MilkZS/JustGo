d31921aba631163cf2be1ec2a1b2cd07ee64fd62
zuoqi <774765813@qq.com>
2018-04-21 17:00:14 +0800

    Single class for place info

---
+++ app/src/main/java/com/milkzs/android/wheretotravel/Base/PlaceListInfo.java
@@ -0,0 +1,22 @@
+package com.milkzs.android.wheretotravel.Base;
+
+import java.util.ArrayList;
+
+/**
+ * Created by milkdz on 2018/4/21.
+ */
+
+public class PlaceListInfo {
+
+    private String placeName ;
+    private String summary ;
+    private String address;
+    private String openTime;
+
+    /** pictures */
+    private ArrayList<String> picListUrl ;
+    private ArrayList<String> picListSmallUrl;
+
+
+
+}
