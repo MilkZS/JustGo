64270cdb507ec532c55a156326f50000b3102f56
zuoqi <774765813@qq.com>
2018-04-22 18:10:40 +0800

    Add format class

---
+++ app/src/main/java/com/milkzs/android/wheretotravel/Tool/FormatData.java
@@ -0,0 +1,27 @@
+package com.milkzs.android.wheretotravel.Tool;
+
+import android.content.Context;
+
+import com.milkzs.android.wheretotravel.R;
+
+
+/**
+ * Created by milkdz on 2018/4/22.
+ */
+
+public class FormatData {
+    /**
+     * format price such as "元/人"
+     *
+     * @param price
+     * @param context
+     * @return
+     */
+    public static String formatPrice(String price, Context context){
+        if(price.equals("") || price == null){
+            return context.getResources().getString(R.string.format_price_null);
+        }
+        return price + context.getResources().getString(R.string.format_price);
+    }
+
+}
