d886b21833e8e5a62bc449d2f261933599281849
zuoqi <774765813@qq.com>
2018-04-25 22:13:07 +0800

    Update detail activity #2

--- app/src/main/java/com/milkzs/android/wheretotravel/PlaceDetailActivity.java
+++ app/src/main/java/com/milkzs/android/wheretotravel/PlaceDetailActivity.java
@@ -1,13 +1,31 @@
 package com.milkzs.android.wheretotravel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.milkzs.android.wheretotravel.Base.BaseInfo;
+import com.milkzs.android.wheretotravel.Base.PlaceListInfo;
 
 public class PlaceDetailActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_detail);
+
+        Intent intent = getIntent();
+        PlaceListInfo placeListInfo = (PlaceListInfo) intent.getSerializableExtra(
+                BaseInfo.IntentFlag.FLAG_ARRAY_LIST_DETAIL);
+
+
+
+    }
+
+    private void bindDataToUI(){
+
     }
 }
