8bacd1cac8caafa3f17a2db3e377f0adf31411a1
zuoqi <774765813@qq.com>
2018-05-05 10:39:41 +0800

    build select menu #5

--- app/src/main/java/com/milkzs/android/wheretotravel/adapter/PlaceAdapter.java
+++ app/src/main/java/com/milkzs/android/wheretotravel/adapter/PlaceAdapter.java
@@ -1,7 +1,9 @@
 package com.milkzs.android.wheretotravel.adapter;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 
 import com.milkzs.android.wheretotravel.Base.PlaceListInfo;
 import com.milkzs.android.wheretotravel.R;
+import com.milkzs.android.wheretotravel.db.PlaceContract;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -20,35 +23,71 @@ import java.util.ArrayList;
 
 public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.MyPlaceAdapterHolder> {
 
+    private String TAG = "PlaceAdapter";
+
     private ArrayList<PlaceListInfo> placeListInfos;
+    private Cursor mCursor;
     private Context context;
-    private ClickTranform clickTranform ;
+    private ClickTranform clickTranform;
+    private int mode = MODE_LIST;
 
+    public final static int MODE_LIST = 0;
+    public final static int MODE_LOG = 1;
 
-    public PlaceAdapter(ClickTranform clickTranform) {
+    public PlaceAdapter(ClickTranform clickTranform, int mode) {
         this.clickTranform = clickTranform;
+        this.mode = mode;
     }
 
     @Override
     public MyPlaceAdapterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         context = parent.getContext();
-        int recyclerList = R.layout.recycler_list_card_view;
+        int List = layoutList(mode);
         LayoutInflater layoutInflater = LayoutInflater.from(context);
-        View view = layoutInflater.inflate(recyclerList,parent,false);
+        View view = layoutInflater.inflate(List, parent, false);
         return new MyPlaceAdapterHolder(view);
+    }
+
+    /**
+     * Get diff mode by mode
+     *
+     * @param mode
+     * @return
+     */
+    private int layoutList(int mode) {
+        switch (mode) {
+            case MODE_LIST: {
+                return R.layout.recycler_list_card_view;
+            }
+            case MODE_LOG: {
+                return R.layout.recycler_list_log;
+            }
+        }
+        return R.layout.recycler_list_card_view;
     }
 
     @Override
     public void onBindViewHolder(MyPlaceAdapterHolder holder, int position) {
-        holder.bindToView(position,context);
+        if(mCursor != null){
+            mCursor.moveToPosition(position);
+            Log.d(TAG,"run here test and position is " + position);
+        }
+        holder.bindToView(position, context,mCursor);
     }
 
     @Override
     public int getItemCount() {
-        if(placeListInfos == null){
-            return 0;
+        if(mode == MODE_LIST){
+            if(placeListInfos != null){
+                return placeListInfos.size();
+            }
+        }else if(mode == MODE_LOG){
+            if(mCursor != null){
+                Log.d(TAG,"recycler cursor count is " + mCursor.getCount());
+                return mCursor.getCount();
+            }
         }
-        return placeListInfos.size();
+        return 0;
     }
 
     /**
@@ -56,12 +95,16 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.MyPlaceAdapt
      *
      * @param arrayList
      */
-    public void swapData(ArrayList<PlaceListInfo> arrayList){
-        this.placeListInfos = arrayList;
+    public void swapData(ArrayList<PlaceListInfo> arrayList,Cursor mCursor,int mode) {
+        if(mode == MODE_LIST){
+            this.placeListInfos = arrayList;
+        }else if(mode == MODE_LOG){
+            this.mCursor = mCursor;
+        }
         notifyDataSetChanged();
     }
 
-    class MyPlaceAdapterHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    class MyPlaceAdapterHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private ImageView mainPic;
         private TextView nameText;
@@ -72,15 +115,35 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.MyPlaceAdapt
         private int position;
         private PlaceListInfo placeListInfo;
 
+        private TextView log_time;
+        private TextView log_name;
+
         public MyPlaceAdapterHolder(View itemView) {
             super(itemView);
-            itemView.setOnClickListener(this);
-            mainPic = itemView.findViewById(R.id.image_main);
-            nameText = itemView.findViewById(R.id.text_view_name);
-            summaryText = itemView.findViewById(R.id.text_view_summary);
-            addressText = itemView.findViewById(R.id.text_view_address);
-            timeText = itemView.findViewById(R.id.text_view_open_time);
-            priceText = itemView.findViewById(R.id.text_view_price);
+            initView(itemView);
+        }
+
+        /**
+         * init view by mode
+         *
+         * @param itemView
+         */
+        private void initView(View itemView){
+            switch (mode){
+                case MODE_LIST:{
+                    itemView.setOnClickListener(this);
+                    mainPic = itemView.findViewById(R.id.image_main);
+                    nameText = itemView.findViewById(R.id.text_view_name);
+                    summaryText = itemView.findViewById(R.id.text_view_summary);
+                    addressText = itemView.findViewById(R.id.text_view_address);
+                    timeText = itemView.findViewById(R.id.text_view_open_time);
+                    priceText = itemView.findViewById(R.id.text_view_price);
+                }break;
+                case MODE_LOG:{
+                    log_time = itemView.findViewById(R.id.log_time);
+                    log_name = itemView.findViewById(R.id.log_name);
+                }break;
+            }
         }
 
         /**
@@ -88,24 +151,38 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.MyPlaceAdapt
          *
          * @param position data index of array list
          */
-        public void bindToView(int position,Context context){
+        public void bindToView(int position, Context context,Cursor cursor) {
             this.position = position;
-            placeListInfo = placeListInfos.get(position);
-            Picasso.with(context).load(placeListInfo.getMainPicUri()).into(mainPic);
-            nameText.setText(placeListInfo.getPlaceName());
-            summaryText.setText(placeListInfo.getSummary());
-            addressText.setText(placeListInfo.getAddress());
-            timeText.setText(placeListInfo.getOpenTime());
-            priceText.setText(placeListInfo.getPrice());
+
+            switch (mode){
+                case MODE_LIST:{
+                    placeListInfo = placeListInfos.get(position);
+                    Picasso.with(context).load(placeListInfo.getMainPicUri()).into(mainPic);
+                    nameText.setText(placeListInfo.getPlaceName());
+                    summaryText.setText(placeListInfo.getSummary());
+                    addressText.setText(placeListInfo.getAddress());
+                    timeText.setText(placeListInfo.getOpenTime());
+                    priceText.setText(placeListInfo.getPrice());
+                }break;
+                case MODE_LOG:{
+                    Log.d(TAG,"each cursor time content is " + cursor.getString(
+                            cursor.getColumnIndex(PlaceContract.PlaceBase.COLUMN_PLACE_NAME)));
+                    log_name.setText(cursor.getString(
+                            cursor.getColumnIndex(PlaceContract.PlaceBase.COLUMN_PLACE_NAME)));
+
+                    log_time.setText(cursor.getString(
+                            cursor.getColumnIndex(PlaceContract.PlaceBase.COLUMN_PLACE_TIME)));
+                }break;
+            }
         }
 
         @Override
         public void onClick(View v) {
-            clickTranform.onClick(position,placeListInfo);
+            clickTranform.onClick(position, placeListInfo);
         }
     }
 
-    public interface ClickTranform{
-        void onClick(int position,PlaceListInfo placeListInfo);
+    public interface ClickTranform {
+        void onClick(int position, PlaceListInfo placeListInfo);
     }
 }
