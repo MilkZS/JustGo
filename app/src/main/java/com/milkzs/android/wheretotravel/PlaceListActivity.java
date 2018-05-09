6657cb1d9ca3abaac5c988cd7b8509fb2946fe64
zuoqi <774765813@qq.com>
2018-05-09 20:32:12 +0800

    fix google admob banner #14

--- app/src/main/java/com/milkzs/android/wheretotravel/PlaceListActivity.java
+++ app/src/main/java/com/milkzs/android/wheretotravel/PlaceListActivity.java
@@ -1,13 +1,14 @@
 package com.milkzs.android.wheretotravel;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -17,6 +18,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.milkzs.android.wheretotravel.Base.BaseInfo;
 import com.milkzs.android.wheretotravel.Base.PlaceListInfo;
 import com.milkzs.android.wheretotravel.Task.QueryDataTask;
@@ -24,8 +28,9 @@ import com.milkzs.android.wheretotravel.Titanic.TitanicTextView;
 import com.milkzs.android.wheretotravel.adapter.PlaceAdapter;
 import com.milkzs.android.wheretotravel.db.PlaceContract;
 
+import com.google.android.gms.ads.MobileAds;
 
-public class PlaceListActivity extends AppCompatActivity implements PlaceAdapter.ClickTranform {
+public class PlaceListActivity extends AppCompatActivity implements PlaceAdapter.ClickTranform, LoaderManager.LoaderCallbacks<Cursor> {
 
     private String TAG = "PlaceListActivity";
     private boolean DBG = false;
@@ -39,17 +44,31 @@ public class PlaceListActivity extends AppCompatActivity implements PlaceAdapter
     private GridLayoutManager gridLayoutManager;
     private Toolbar mToolbar;
 
-    private int position = 0;
+    private int position = RecyclerView.NO_POSITION;
     private String POSITION_FLAG = "flag_position";
+    private String SHARED_FILE = "share_file";
+
+    private AdView mAdView;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_list);
 
-        if(savedInstanceState != null){
-            position = savedInstanceState.getInt(POSITION_FLAG);
-        }
+        MobileAds.initialize(this, getResources().getString(R.string.sign_build_id));
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId(getResources().getString(R.string.sign_app_id));
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+        sharedPreferences = getSharedPreferences(SHARED_FILE, MODE_PRIVATE);
+        position = sharedPreferences.getInt(POSITION_FLAG, 0);
+        Log.d(TAG,"get position is " + position);
 
         mToolbar = findViewById(R.id.list_toolbar);
         setSupportActionBar(mToolbar);
@@ -65,6 +84,13 @@ public class PlaceListActivity extends AppCompatActivity implements PlaceAdapter
         refreshMode(PlaceAdapter.MODE_LIST);
     }
 
+
+
+    /**
+     * chose mode to run
+     *
+     * @param mode
+     */
     private void refreshMode(int mode) {
         switch (mode) {
             case PlaceAdapter.MODE_LIST: {
@@ -92,7 +118,7 @@ public class PlaceListActivity extends AppCompatActivity implements PlaceAdapter
                 if (queryDataTask != null) {
                     queryDataTask.cancel(true);
                 }
-                getSupportLoaderManager().initLoader(0, null, mCallback);
+                getSupportLoaderManager().initLoader(0, null, this);
             }
             break;
         }
@@ -125,26 +151,49 @@ public class PlaceListActivity extends AppCompatActivity implements PlaceAdapter
         return true;
     }
 
-    private final LoaderManager.LoaderCallbacks<Cursor> mCallback = new LoaderManager.LoaderCallbacks<Cursor>() {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_FILE, MODE_PRIVATE);
+        position = ((GridLayoutManager)recyclerView.getLayoutManager()).findLastVisibleItemPosition();
+        Toast.makeText(this,"position is " + position,Toast.LENGTH_SHORT).show();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(POSITION_FLAG, position);
+        editor.apply();
+        editor.commit();
+        super.onSaveInstanceState(outState);
+    }
 
-        @Override
-        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-            Uri placeUri = PlaceContract.PlaceBase.CONTENT_BASE;
-            String order = PlaceContract.PlaceBase.COLUMN_PLACE_TIME + " ASC";
-            return new CursorLoader(
-                    getBaseContext(), placeUri, new String[]{" * "}, null, null, order);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"resume position is " + position);
+        recyclerView.smoothScrollToPosition(position);
+        recyclerView.getLayoutManager().scrollToPosition(position);
+    }
 
-        @Override
-        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-            Log.d(TAG,"run here times ");
-            placeAdapter.swapData(null, data, PlaceAdapter.MODE_LOG);
-        }
 
-        @Override
-        public void onLoaderReset(Loader<Cursor> loader) {
-            placeAdapter.swapData(null, null, PlaceAdapter.MODE_LOG);
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri placeUri = PlaceContract.PlaceBase.CONTENT_BASE;
+        String order = PlaceContract.PlaceBase.COLUMN_PLACE_TIME + " ASC";
+        return new CursorLoader(
+                getBaseContext(), placeUri, new String[]{" * "}, null, null, order);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.d(TAG, "run here times ");
+        placeAdapter.swapData(null, data, PlaceAdapter.MODE_LOG);
+        if (position == RecyclerView.NO_POSITION) {
+            position = 0;
         }
-    };
+        Log.d(TAG,"smooth scroll to position is " + position);
+        recyclerView.smoothScrollToPosition(position);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        placeAdapter.swapData(null, null, PlaceAdapter.MODE_LOG);
+    }
 
 }
