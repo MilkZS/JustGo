d39e57102f13e159ed39e3181edbeb10528a0a40
zuoqi <2753652147@qq.com>
2018-07-01 17:46:02 +0800

    Scale image #23

---
+++ app/src/main/java/com/milkzs/android/wheretotravel/ZooImageImpl/ZooImageActivity.java
@@ -0,0 +1,29 @@
+package com.milkzs.android.wheretotravel.ZooImageImpl;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.milkzs.android.wheretotravel.Base.BaseInfo;
+import com.milkzs.android.wheretotravel.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ZooImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoo_image);
+
+        Intent intent = getIntent();
+        int position = intent.getIntExtra(BaseInfo.IntentFlag.FLAG_PICTURE_POSITION,0);
+        ArrayList<Uri> list = intent.getParcelableArrayListExtra(BaseInfo.IntentFlag.FLAG_PICTURE_LIST);
+
+        ImageView imageView = findViewById(R.id.zoo_image_view);
+        Picasso.with(this).load(list.get(position)).into(imageView);
+    }
+}
