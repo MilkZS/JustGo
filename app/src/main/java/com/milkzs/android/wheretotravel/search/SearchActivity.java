93ebe81ca99a2eda7ade71d0500b44a7cce9cb7d
zuoqi <2753652147@qq.com>
2018-07-15 21:02:35 +0800

    delete unused #11

--- app/src/main/java/com/milkzs/android/wheretotravel/search/SearchActivity.java
+++ app/src/main/java/com/milkzs/android/wheretotravel/search/SearchActivity.java
@@ -5,31 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 import com.milkzs.android.wheretotravel.Base.BaseInfo;
 import com.milkzs.android.wheretotravel.R;
-import com.milkzs.android.wheretotravel.Tool.AnalysisJsonData;
-import com.milkzs.android.wheretotravel.Tool.DataRequest;
 import com.milkzs.android.wheretotravel.search.task.SearchNameTask;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.CountDownLatch;
 
 public class SearchActivity extends AppCompatActivity implements SearchView.SearchViewListener {
 
     private String TAG = "SearchActivity";
     private ArrayAdapter<String> autoCompleteDataAdapter;
     private ArrayList<String> autoCompleteData = new ArrayList<>();
-    private SearchView searchView;
     private SearchNameTask searchNameTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        searchView = findViewById(R.id.search_define_view);
+        SearchView searchView = findViewById(R.id.search_define_view);
         searchView.setClickListener(this);
         autoCompleteDataAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_expandable_list_item_1
@@ -44,10 +38,8 @@ public class SearchActivity extends AppCompatActivity implements SearchView.Sear
             searchNameTask.cancel(true);
             searchNameTask = null;
         }
-        searchNameTask = new SearchNameTask(this,autoCompleteDataAdapter,searchView);
+        searchNameTask = new SearchNameTask(this,autoCompleteDataAdapter);
         searchNameTask.execute(text);
-        
-        //Toast.makeText(this,"refresh",Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/milkzs/android/wheretotravel/search/task/SearchNameTask.java
+++ app/src/main/java/com/milkzs/android/wheretotravel/search/task/SearchNameTask.java
@@ -6,52 +6,45 @@ import android.widget.ArrayAdapter;
 
 import com.milkzs.android.wheretotravel.Tool.AnalysisJsonData;
 import com.milkzs.android.wheretotravel.Tool.DataRequest;
-import com.milkzs.android.wheretotravel.search.SearchView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * Created by alan on 2018/7/14.
+ * Created by milkz on 2018/7/14.
  */
 
-public class SearchNameTask extends AsyncTask<String,Void,ArrayList<String>> {
+public class SearchNameTask extends AsyncTask<String, Void, ArrayList<String>> {
 
     private Context context;
     private ArrayAdapter<String> autoDataAdapter;
-    private SearchView searchView;
 
-    public SearchNameTask(Context context, ArrayAdapter<String> autoDataAdapter, SearchView searchView) {
+    public SearchNameTask(Context context, ArrayAdapter<String> autoDataAdapter) {
         this.context = context;
         this.autoDataAdapter = autoDataAdapter;
-        this.searchView = searchView;
     }
 
     @Override
     protected ArrayList<String> doInBackground(String... strings) {
         String sText = "";
-        if(strings.length != 0){
+        if (strings.length != 0) {
             sText = strings[0];
         }
-
         try {
             String jsonString = DataRequest.getResponseFromHttpUrl(
                     DataRequest.buildURIForSearchKeyword(sText));
-            return AnalysisJsonData.getNameFromJson(jsonString,context);
-
+            return AnalysisJsonData.getNameFromJson(jsonString, context);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
     @Override
     protected void onPostExecute(ArrayList<String> strings) {
-        if(strings != null){
-          autoDataAdapter.clear();
-          autoDataAdapter.addAll(strings);
-          //  searchView.setAutoCompleteAdapter(autoDataAdapter);
+        if (strings != null) {
+            autoDataAdapter.clear();
+            autoDataAdapter.addAll(strings);
             autoDataAdapter.notifyDataSetChanged();
         }
     }
