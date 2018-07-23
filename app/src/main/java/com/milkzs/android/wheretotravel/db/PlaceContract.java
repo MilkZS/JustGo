6e8f7c6f88df984cd3ab7b13189b43e96e05b306
zuoqi <2753652147@qq.com>
2018-07-23 22:10:58 +0800

    create DB #29

--- app/src/main/java/com/milkzs/android/wheretotravel/db/PlaceContract.java
+++ app/src/main/java/com/milkzs/android/wheretotravel/db/PlaceContract.java
@@ -33,22 +33,36 @@ public class PlaceContract {
         };
     }
 
-    public static final class SearchTAG implements BaseColumns{
-        public static String SEARCH_INFO = "searchTag";
-        public static String TABLE_NAME = "where_to_travel_search_tag";
+    public static final class SceneBase implements BaseColumns{
+        public static String SCENE_INFO = "scene";
+        public static String TABLE_NAME = "where_to_travel_scene";
 
-        public static final Uri CONTENT_BASE = CONTENT_BASE_URI.buildUpon()
-                .appendPath(SEARCH_INFO).build();
+        public static final Uri CONTENT_BASE = CONTENT_BASE_URI
+                .buildUpon().appendPath(SCENE_INFO).build();
 
-        public static String COLUMN_ID = "search_tag_id";
-        public static String COLUMN_TAG = "search_tag_tags";
-        public static String COLUMN_USER_ID = "search_tag_userId";
+        public static String COLUMN_SCENE_ID = "scene_id";
+        public static String COLUMN_SCENE_NAME = "scene_name";
+        public static String COLUMN_SCENE_SUMMERY = "scene_summery";
+        public static String COLUMN_SCENE_PRICE = "scene_price";
+        public static String COLUMN_SCENE_OPEN_TIME = "scene_open_time";
+        public static String COLUMN_SCENE_ADDRESS = "scene_address";
+        public static String COLUMN_SCENE_PICTURE_SRC = "scene_picture_src";
+        public static String COLUMN_SCENE_DISCOUNT = "scene_discount";
+        public static String COLUMN_SCENE_ATTENTION = "scene_attention";
+        public static String COLUMN_SCENE_CONTENT = "scene_content";
 
         public static String[] QUERY_ENTRY = {
                 _ID,
-                COLUMN_ID,
-                COLUMN_TAG,
-                COLUMN_USER_ID
+                COLUMN_SCENE_ID,
+                COLUMN_SCENE_NAME,
+                COLUMN_SCENE_SUMMERY,
+                COLUMN_SCENE_PRICE,
+                COLUMN_SCENE_OPEN_TIME,
+                COLUMN_SCENE_ADDRESS,
+                COLUMN_SCENE_PICTURE_SRC,
+                COLUMN_SCENE_DISCOUNT,
+                COLUMN_SCENE_ATTENTION,
+                COLUMN_SCENE_CONTENT
         };
     }
 }
